db7450dc80182a22479829c9cba2b4d7ad3f04c0
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-02-17 17:40:16 -0300

    Blog Pessoal

---
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,72 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+	@Entity     // entidade
+	@Table(name = "postagem")          // criacao de tabela
+	public class Postagem {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //chave primaria
+	private long id;
+	
+	
+	@NotNull
+	@Size(min = 5, max = 100)    //minimo e maximo de letras
+	private String titulo;
+	
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String texto;
+	
+	
+	@Temporal(TemporalType.TIMESTAMP)   //data e horas juntos
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
