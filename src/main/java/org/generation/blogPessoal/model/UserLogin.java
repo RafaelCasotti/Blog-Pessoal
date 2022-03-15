6601ae414065068e240b87d78cf0c825544adcb0
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-03-15 14:14:37 -0300

    @RafaelCasotti [Task: Correção Blog Pessoal - Por @GustavoBoaz]

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -2,7 +2,7 @@ package org.generation.blogPessoal.model;
 
 public class UserLogin {
 	
-	private long id;
+	private Long id;
 
 	private String nome;
 
@@ -18,7 +18,7 @@ public class UserLogin {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
